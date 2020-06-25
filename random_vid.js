f192c1981f7083b84056a3432345e83707cbd5ba
betsy <8404974+betsy@users.noreply.github.com>
2020-06-25 18:49:09 -0400

    put js in its own file bc slightly less gross

---
+++ random_vid.js
@@ -0,0 +1,25 @@
+const threshold = 450000000; // generate random video ids between 0 and this value
+
+function tryUntilSuccess(vp, retries)
+{
+	if (!retries) return 0;
+	date = new Date();
+	new_video_id = Math.round(Math.random(date.getMilliseconds())*threshold);
+	console.log("new video id: "+new_video_id+". retries left: "+retries);
+    return vp.loadVideo("https://player.vimeo.com/video/"+new_video_id).catch(rej => {
+		console.log("reject: "+rej);
+		tryUntilSuccess(vp, retries-1);
+    });
+}
+
+
+var videoPlayer;
+window.onload = function(e){
+	var options = {
+		url: "https://player.vimeo.com/video/350477474/", // default, blank video
+		width: 800
+	};
+
+	videoPlayer = new Vimeo.Player('myVideo', options);;
+	tryUntilSuccess(videoPlayer, 30);
+}
